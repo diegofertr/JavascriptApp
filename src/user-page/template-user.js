14c7f96de865bdd49a0b8ea6963c21f312dc9167
diegofertr <diegonano09@gmail.com>
2017-07-16 21:36:31 -0400

    Bug fixes user page styles

--- src/user-page/template-user.js
+++ src/user-page/template-user.js
@@ -6,7 +6,7 @@ var layout = require('../layout');
 var translate = require('../translate');
 
 module.exports = function userPageTemplate(user) {
-	var el = yo`<div class="container user-page">
+	var el = yo`<div class="container userpage">
 	<div class="row">
 		<div class="col s12 m10 offset-m1 l12 center-align heading">
 			<div class="row">
@@ -22,13 +22,13 @@ module.exports = function userPageTemplate(user) {
 		<div class="row">
 					${user.pictures.map(function (picture) {
 						return yo`
-						<div class="col s6 m12 l4">
-						<div class="picture-container">
-						<img src="${picture.src}" class="picture" />
-						<div class="likes">
-						<i class="fa fa-heart"></i> ${picture.likes}
-						</div>
-						</div>
+						<div class="col s12 m12 l4">
+							<div class="picture-container">
+								<img src="${picture.src}" class="picture" />
+								<div class="likes">
+									<i class="fa fa-heart"></i> ${picture.likes}
+								</div>
+							</div>
 						</div>
 						`
 					})}
